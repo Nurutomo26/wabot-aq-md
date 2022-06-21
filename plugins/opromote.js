19bfc9e4d8ab8235a3a9e11ba74b23a680e256eb
Nurutomo26 <103919858+Nurutomo26@users.noreply.github.com>
2022-06-21 17:56:07 +0800

    Update opromote.js

--- plugins/opromote.js
+++ plugins/opromote.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, participants }) => {
   let members = participants.filter(member => !member.isAdmin).map(member => member.jid)
   let users = m.mentionedJid.filter(user => members.includes(user))
-  for (let user of users) await conn.groupMakeAdmin(m.chat, [user]).catch(console.log)
+  for (let user of users) await conn.groupParticipantsUpdate(m.chat, [user], 'promote').catch(console.log)
 }
 handler.help = ['promote','admin','^'].map(v => 'o' + v + ' @user')
 handler.tags = ['owner']
